9922c9611538306dc6a8f740adef3573e8065ccc
Andrew Richardson <photographyenthusiastabr@gmail.com>
2021-02-14 15:00:28 -0500

    1

--- aaaaProject/Program.cs
+++ aaaaProject/Program.cs
@@ -9,16 +9,6 @@ namespace aaaaProject
             Console.WriteLine("Hello World!");
 
             //1
-            //2
-            //3
-            //4
-            //5
-            //6
-            //7
-            //8
-            //9
-            //10
-            //11
         }
     }
 }
